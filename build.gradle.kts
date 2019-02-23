e96847f8748744c7c8aa1d01ed988e63c09aedc8
Joseph Ivie <josephivie@gmail.com>
2019-02-22 23:06:39 -0700

    IntelliJ configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ val versions = Properties().apply {
 group = "com.lightningkite"
 version = versions.getProperty(project.name)
 
-project.doNotPublishMetadata()
 kotlin {
 
     sources(tryTargets = KTarget.allExceptAndroid - KTarget.wasm32) {
@@ -75,71 +74,3 @@ publishing {
         }
     }
 }
-
-
-/*import com.lightningkite.konvenience.gradle.*
-import java.util.Properties
-
-plugins {
-    kotlin("multiplatform") version "1.3.21"
-    `maven-publish`
-    id("com.lightningkite.konvenience") version "+"
-}
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-    maven("https://dl.bintray.com/lightningkite/com.lightningkite.krosslin")
-}
-
-val versions = Properties().apply {
-    load(project.file("versions.properties").inputStream())
-}
-
-group = "com.lightningkite"
-version = versions.getProperty(project.name)
-
-kotlin {
-    sources(KTarget.common) {
-        main {
-            mpp(standardLibrary)
-            mpp(projectOrMavenDashPlatform("com.ivieleague", "kommon", versions.getProperty("kommon")))
-        }
-        test {
-            mpp(testing)
-            mpp(testingAnnotations)
-        }
-        KTarget.ios.sources{}
-        KTarget.js.sources{}
-        KTarget.jvm.sources{}
-        KTarget.mingwX64.sources {}
-        KTarget.posix.without(KTarget.ios).sources {}
-    }
-}
-
-publishing {
-    repositories {
-        bintray(
-                project = project,
-                organization = "lightningkite",
-                repository = "com.lightningkite.krosslin"
-        )
-    }
-
-    appendToPoms {
-        github("lightningkite", project.name)
-        licenseMIT()
-        developers {
-            developer {
-                id.set("UnknownJoe796")
-                name.set("Joseph Ivie")
-                email.set("joseph@lightningkite.com")
-                timezone.set("America/Denver")
-                roles.set(listOf("architect", "developer"))
-                organization.set("Lightning Kite")
-                organizationUrl.set("http://www.lightningkite.com")
-            }
-        }
-    }
-}
-
